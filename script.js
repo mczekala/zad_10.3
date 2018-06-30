0e5f3f2bec7bbad35bce1c39dee3f93333fe4afb
Marcin Czekala <marcin_czekala@icloud.com>
2018-06-30 17:49:42 +0200

    add

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function(){
-	var carouselList = $('#carousel ul');
-	var carouselI = $('#carousel ol li');
-	var index = 0;
+	var carouselImages = $('#carousel ul');
+	var carouselIndicators = $('#carousel ol li');
+
 	$('#buttonL').click(function() {changeSlideL()});
 	$('#buttonR').click(function() {changeSlideR()});
 	var indicator_1 = $('#index-1').click(function() {move(1)});
@@ -10,49 +10,65 @@ $(function(){
 	var indicator_4 = $('#index-4').click(function() {move(4)});
 	var indicator_5 = $('#index-5').click(function() {move(5)});
 	
-	function changeSlideL() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlideL);
-	}
 	function changeSlideR() {
-		carouselList.animate({'marginLeft':400}, 500, moveFirstSlideR);
-	}
-	function moveFirstSlideL() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-		index--;
-		changeToFull();
+		carouselImages.animate({'marginLeft':-400}, 500, moveFirstSlideR);
 	}
 	function moveFirstSlideR() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
+		var firstItem = carouselImages.find('li:first');
+		var lastItem = carouselImages.find('li:last');
+		lastItem.after(firstItem);
+		carouselImages.css({marginLeft:0});
+		changeToFull();
+	}
+	function changeSlideL() {
+		carouselImages.animate({'marginLeft':400}, 500, moveFirstSlideL);
+	}
+	function moveFirstSlideL() {
+		var firstItem = carouselImages.find('li:first');
+		var lastItem = carouselImages.find('li:last');
 		firstItem.before(lastItem);
-		carouselList.css({marginLeft:0});
-		index++;
+		carouselImages.css({marginLeft:0});
 		changeToFull();
 	}
 	function changeToFull() {
-		changeToEmpty(index-1);
-		changeToEmpty(index+1);
+		var index=$('#carousel ul li:first').attr('id');
+		changeToEmpty();
 		if (index<0) {
 			index=index+5;
 		}
 		index=index%5;
-		carouselI.eq(index).html('<li><i class="fas fa-circle"></i></li>');
+		carouselIndicators.eq(index).html('<li><i class="fas fa-circle"></i></li>');
 	}
 	function changeToEmpty(index) {
+		var index=parseInt($('#carousel ul li:first').attr('id'))-1;
 		if (index<0) {
 			index=index+5;
 		}
 		index=index%5;
-		carouselI.eq(index).html('<li><i class="far fa-circle"></i></li>');
+		carouselIndicators.eq(index).html('<li><i class="far fa-circle"></i></li>');
+		index+=2;
+		if (index<0) {
+			index=index+5;
+		}
+		index=index%5;
+		carouselIndicators.eq(index).html('<li><i class="far fa-circle"></i></li>');
 	}
 	function move(max) {
-		for(var i=0;i<max;i++) {
+		var actual=checkActual();
+		var distance = max-actual;
+		if (distance<0) {
+			distance=distance+5;
+		}
+		distance = distance%5;
+		for(var i=0;i<distance;i++) {
 			changeSlideR();
 		}
-		console.log(indicator_1);
-		console.log(indicator_5);
+	}
+	function checkActual() {
+		var actual = parseInt($('#carousel ul li:first').attr('id'));
+			if (actual<0) {
+				actual=actual+5;
+		}
+		return (actual%5)+1;
 	}
 });
